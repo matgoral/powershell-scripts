9fad9dee8b7b74077a4902ee951993331bc47d45
Mateusz Góral <mat.goral7@gmail.com>
2021-06-10 14:49:20 +0200

    improvements

--- find_and_replace_string/find_replace_string.ps1
+++ find_and_replace_string/find_replace_string.ps1
@@ -1,13 +1,52 @@
 #Script to find all word/string in all files in directory and change it
 
+#Start Transcript
+$LogFileOutput = ".\Logs\LogOutput.txt"
+Start-Transcript -path $LogFileOutput
+
 Get-Date
-Get-ChildItem -Path "pathToDir" -Recurse | Select-String -Pattern "Pattern" |  Select Line, Path | Export-Csv -path Output.csv
 
-Import-Csv .\Output.csv | ForEach-Object {
+Write-Output "Start - searching for files"
 
-     $file = $_.Path
-     Copy-Item $file -Destination "$file.back"
-     (Get-Content $_.Path).replace('Pattern', 'NewURL') | Set-Content $_.Path
+Get-ChildItem -Path "path" -Recurse | Select-String -Pattern "OLD_STRING" |  Select Line, Path | Export-Csv -path Findings.csv
 
+Write-Output "Found:"
+Import-Csv .\Findings.csv | Measure-Object | Select Count
+Write-Output "------------------------------"
+Get-Date
+
+Write-Output "Starting swap string in files"
+$errors=0
+$counter=0
+
+Import-Csv .\Findings.csv | ForEach-Object {
+    
+    $file = $_.Path
+#Create backup file ---- WARNING! In case of a large number of files, check the free space on the disk.
+     #
+     #Copy-Item $file -Destination "$file.back"
+#Replace OLD string with NEW
+     (Get-Content $_.Path).replace('OLD_STRING', 'NEW_STRING') | Set-Content $_.Path
+
+     $CHECK = Select-String -Path $file -Pattern "OLD_STRING"
+
+    if ($CHECK -ne $null)
+        {
+            Write-Output "ERROR: $file"
+            $errors++
+        }
+    else
+        {
+            Write-Output "OK: $file"
+            $counter++ 
+        }
 }
+
+Write-Output "ERRORS: $errors"
+Write-Output "OK: $counter"
+
+Write-Output "Script done"
+Write-Output "------------------------------"
 Get-Date
+
+Stop-Transcript
