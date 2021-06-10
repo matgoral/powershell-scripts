c1a1a30bf3a8de7756aac065af1e3241765bdaf2
Mateusz Góral <mat.goral7@gmail.com>
2021-06-10 14:52:55 +0200

    format docs

--- find_and_replace_string/find_replace_string.ps1
+++ find_and_replace_string/find_replace_string.ps1
@@ -16,30 +16,28 @@ Write-Output "------------------------------"
 Get-Date
 
 Write-Output "Starting swap string in files"
-$errors=0
-$counter=0
+$errors = 0
+$counter = 0
 
 Import-Csv .\Findings.csv | ForEach-Object {
     
     $file = $_.Path
-#Create backup file ---- WARNING! In case of a large number of files, check the free space on the disk.
-     #
-     #Copy-Item $file -Destination "$file.back"
-#Replace OLD string with NEW
-     (Get-Content $_.Path).replace('OLD_STRING', 'NEW_STRING') | Set-Content $_.Path
+    #Create backup file ---- WARNING! In case of a large number of files, check the free space on the disk.
+    #
+    #Copy-Item $file -Destination "$file.back"
+    #Replace OLD string with NEW
+    (Get-Content $_.Path).replace('OLD_STRING', 'NEW_STRING') | Set-Content $_.Path
 
-     $CHECK = Select-String -Path $file -Pattern "OLD_STRING"
+    $CHECK = Select-String -Path $file -Pattern "OLD_STRING"
 
-    if ($CHECK -ne $null)
-        {
-            Write-Output "ERROR: $file"
-            $errors++
-        }
-    else
-        {
-            Write-Output "OK: $file"
-            $counter++ 
-        }
+    if ($CHECK -ne $null) {
+        Write-Output "ERROR: $file"
+        $errors++
+    }
+    else {
+        Write-Output "OK: $file"
+        $counter++ 
+    }
 }
 
 Write-Output "ERRORS: $errors"
